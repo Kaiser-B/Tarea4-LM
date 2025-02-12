68296e3f154e59a6964106efb32c2d7a9d199728
VICTOR SANCHEZ <victorssanchez@hotmail.es>
2025-02-12 20:46:00 +0100

    nuevas implementaciones del JS

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const entradaTareas = document.querySelector('#entrada-tarea');
 const tareas = document.querySelector('#tareas');
-const botonAnadir =  document.querySelector('#enviar-tarea');
+const botonAnadir =  document.querySelector('#btn-enviar');
 const lista =  document.querySelector('.lista');
 const btnConsejo =  document.querySelector('#btnConsejo');
 const mensaje =  document.querySelector('#mensaje');
@@ -13,18 +13,28 @@ console.log({entradaTareas});
 function agregarTarea(){
     
     if(entradaTareas.value.trim().length < 1){
-        console.log('No puede estar vacío');
+        alert('Debes introducir alguna Tarea');
     }else{
         const tipoTarea = tareas.value;
         const tareaLista = document.createElement('LI');
         tareaLista.textContent = `${entradaTareas.value} - ${tipoTarea}`;
         const btnEliminar = document.createElement('BUTTON');
         btnEliminar.textContent = 'Eliminar';
-        btnEliminar.classList.add('eliminarLista');
+        btnEliminar.classList.add('btn-eliminar');
 
         tareaLista.appendChild(btnEliminar);
         lista.appendChild(tareaLista);
 
+        entradaTareas.value = '';
+
+        btnEliminar.addEventListener('click', ()=>{
+            
+            lista.removeChild(tareaLista);
+        })
+
+        tareaLista.addEventListener('click', ()=>{
+            tareaLista.classList.add('tachada');
+        })
     }
     
 }
