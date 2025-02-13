db8652ddac76527f84eb81a9b670ac73ed01f99f
Victor Sanchez Sanchez <victorssanchez@hotmail.es>
2025-02-13 10:32:02 +0100

    implementado loader

--- script.js
+++ script.js
@@ -4,10 +4,22 @@ const entradaTareas = document.querySelector('#entrada-tarea');
 const tareas = document.querySelector('#tareas');
 const botonAnadir =  document.querySelector('#btn-enviar');
 const lista =  document.querySelector('.lista');
-const btnConsejo =  document.querySelector('#btnConsejo');
+const btnConsejo =  document.querySelector('#btn-consejo');
 const mensaje =  document.querySelector('#mensaje');
+const modalLoader = document.createElement('DIV');
+const textoLoader = document.createElement('P');
+const imgLoader = document.createElement('IMG');
+const contenedor = document.querySelector('.contenedor');
 
-console.log({entradaTareas});
+modalLoader.classList.add('modal');
+imgLoader.classList.add('imgLoader');
+textoLoader.classList.add('textoLoader');
+
+textoLoader.textContent = 'Añadiendo Tarea...';
+imgLoader.setAttribute('src', 'loader.svg');
+
+modalLoader.appendChild(textoLoader);
+modalLoader.appendChild(imgLoader);
 
 
 function agregarTarea(){
@@ -15,6 +27,7 @@ function agregarTarea(){
     if(entradaTareas.value.trim().length < 1){
         alert('Debes introducir alguna Tarea');
     }else{
+        contenedor.appendChild(modalLoader);
         const tipoTarea = tareas.value;
         const tareaLista = document.createElement('LI');
         tareaLista.textContent = `${entradaTareas.value} - ${tipoTarea}`;
@@ -22,8 +35,12 @@ function agregarTarea(){
         btnEliminar.textContent = 'Eliminar';
         btnEliminar.classList.add('btn-eliminar');
 
-        tareaLista.appendChild(btnEliminar);
-        lista.appendChild(tareaLista);
+        
+        setTimeout(() => {
+            tareaLista.appendChild(btnEliminar);
+            lista.appendChild(tareaLista);
+            contenedor.removeChild(modalLoader);
+        }, 1500);
 
         entradaTareas.value = '';
 
@@ -39,4 +56,17 @@ function agregarTarea(){
     
 }
 
+function consejoMotivacional(){
+
+    fetch('https://api.adviceslip.com/advice')
+    .then(respuesta=> respuesta.json())
+    .then(consejo => {
+        mensaje.textContent = consejo.slip.advice;})
+    .catch((error)=>{
+        console.error("Error", error)
+    });
+
+}
+
 botonAnadir.addEventListener('click', agregarTarea);
+btnConsejo.addEventListener('click', consejoMotivacional);
